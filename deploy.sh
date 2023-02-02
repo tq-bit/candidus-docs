fed9dae6ea28f94f05701c4b65724b8c7c21709f
q-bit <t.quante@outlook.com>
2023-02-02 14:45:12 +0100

    script: update deploy URL

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:tq-bit/vitepress-components.git master:gh-pages
+git push -f git@github.com:tq-bit/candidus-docs.git master:gh-pages
 
 cd -
